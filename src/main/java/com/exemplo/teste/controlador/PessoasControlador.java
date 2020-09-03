818a1ab580398b2c2a2107d019e18dbeda4055d0
13jaque <jaquelinegidelfonso@gmail.com>
2020-09-03 18:57:00 -0300

    ajuste no banco

--- src/main/java/com/exemplo/teste/controlador/PessoasControlador.java
+++ src/main/java/com/exemplo/teste/controlador/PessoasControlador.java
@@ -3,7 +3,6 @@ package com.exemplo.teste.controlador;
 import java.util.List;
 import java.util.Optional;
 
-//import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,39 +16,41 @@ import org.springframework.web.bind.annotation.RestController;
 import com.exemplo.teste.entidades.Pessoas;
 import com.exemplo.teste.repositorio.PessoasRepositorio;
 
+import javax.validation.Valid;
+
 
 @RestController
 public class PessoasControlador {
 
     @Autowired
-    private PessoasControlador _pessoasRepositorio;
+    private PessoasRepositorio pessoasRepositorio;
 
     @RequestMapping (value = "/pessoas", method = RequestMethod.GET)
     public List<Pessoas> Get() {
-        return _pessoasRepositorio.findAll();
+        return pessoasRepositorio.findAll();
     }
 
     @RequestMapping (value = "/pessoas{id}",method = RequestMethod.GET)
     public ResponseEntity<Pessoas> GetById(@PathVariable(value = "id") long id) {
-        Optional<Pessoas> pessoas = _pessoasRepositorio.findById(id);
+        Optional<Pessoas> pessoas = pessoasRepositorio.findById(id);
         if(pessoas.isPresent())
             return new ResponseEntity<Pessoas>(pessoas.get(), HttpStatus.OK);
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @RequestMapping(value = "/pessoa/{id}", method = RequestMethod.POST)
-    public Pessoas Post (@Valid @RequestBody Pessoas pessoas) {
-        return _pessoasRepositorio.save(pessoas);
+    @RequestMapping(value = "/pessoas", method = RequestMethod.POST)
+    public Pessoas Post(@Valid @RequestBody Pessoas pessoas) {
+        return pessoasRepositorio.save(pessoas);
     }
 
     @RequestMapping(value = "/pessoas/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Pessoas> Put(@PathVariable(value = "id") long id, @Valid @RequestBody Pessoas newPessoa){
-        Optional<Pessoas> oldPessoas = _pessoasRepositorio.findById(id);
+        Optional<Pessoas> oldPessoas = pessoasRepositorio.findById(id);
         if(oldPessoas.isPresent()){
             Pessoas pessoas = oldPessoas.get();
             pessoas.setNome(newPessoa.getNome());
-            _pessoasRepositorio.save(pessoas);
+            pessoasRepositorio.save(pessoas);
             return new ResponseEntity<Pessoas>(pessoas, HttpStatus.OK);
         }
         else
@@ -58,14 +59,12 @@ public class PessoasControlador {
 
     @RequestMapping(value = "/pessoas/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Object> Delete(@PathVariable(value = "id") long id){
-        Optional<Pessoas> pessoas = _pessoasRepositorio.findById(id);
+        Optional<Pessoas> pessoas = pessoasRepositorio.findById(id);
         if(pessoas.isPresent()){
-            _pessoasRepositorio.delete(pessoas.get());
+            pessoasRepositorio.delete(pessoas.get());
             return new ResponseEntity<>(HttpStatus.OK);
         }
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
-    @Valid
 }
